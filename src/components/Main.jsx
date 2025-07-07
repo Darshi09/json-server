f2c66efd60dcc867975b53c3a4bab07ad56eaafa
Darshi09 <shahdarshi.21.imca@iict.indusuni.ac.in>
2025-07-07 17:15:21 +0530

    new

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router'
 
+
 const Main = () => {
     const [data, setData] = useState([])
     const [productdata, setProductdata] = useState({ img: "", title: "", price: "" })
